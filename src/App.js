7e8c6e724d8c0e15f0fecf019b2ca29be5e9f713
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-04-30 16:34:05 +0200

    React Bootstrap was installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+// Bootstrap components
+import { Button } from "react-bootstrap";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>FAKE FLIX APP</h1>
+      <Button variant="danger">TEST</Button>
+      <Button variant="success">Success</Button>{" "}
     </>
   );
 }
