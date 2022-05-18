c32f1fb39bb5a9ee13df0d24f8c644e729894d1b
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-05-18 12:53:14 +0200

    Minor change: Api call now have variable as a seeking genre

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -22,14 +22,14 @@ export function HomePage() {
       .catch((error) => console.log("error: ", error));
     // set horrorMovies
     await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIES_API_KEY}&with_genres=27`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIES_API_KEY}&with_genres=${MOVIE_GENRES.horror}`
     )
       .then((response) => response.json())
       .then((data) => setHorrorMovies(horrorMovies.concat(data.results)))
       .catch((error) => console.log("error: ", error));
     // set comedyMovies
     await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIES_API_KEY}&with_genres=35`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIES_API_KEY}&with_genres=${MOVIE_GENRES.comedy}`
     )
       .then((response) => response.json())
       .then((data) => setComedyMovies(comedyMovies.concat(data.results)))
@@ -43,18 +43,10 @@ export function HomePage() {
   return (
     <>
       <Hero />
-      {/* {testfield} */}
-      {console.log("RecentMovies: ", recentMovies)}
-      {console.log("HorrorMovies: ", horrorMovies)}
-      {console.log("ComedyMovies: ", comedyMovies)}
-      {/* {testfields end } */}
       <main>
         <MovieRow rowTitle={"Recent Movies"} movies={recentMovies} />
         <MovieRow rowTitle={"Comedy Movies"} movies={comedyMovies} />
-        <MovieRow rowTitle={"Comedy Movies"} movies={horrorMovies} />
-        {/* <MovieRow rowTitle={"Comedy"} genre={MOVIE_GENRES.comedy} />
-        <MovieRow rowTitle={"Horror"} genre={MOVIE_GENRES.horror} />
-        <MovieRow rowTitle={"Sci-Fi"} genre={MOVIE_GENRES["sci-fi"]} /> */}
+        <MovieRow rowTitle={"Horror Movies"} movies={horrorMovies} />
       </main>
     </>
   );
