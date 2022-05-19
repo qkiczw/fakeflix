01e91900259dfe3e55697b362b836312a3c83abe
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-05-19 18:54:45 +0200

    I changed nav in header to a bootstrap component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 // Bootstrap Components
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Navbar, Container, Nav } from "react-bootstrap";
 
 // Assest
 import logo from "../../assets/logo.png";
@@ -13,13 +13,13 @@ import { BsFillBellFill, BsFillPersonFill, BsSearch } from "react-icons/bs";
 const Header = () => {
   return (
     <>
-      <header>
-        <Row className="header">
-          <Col xs={12} md={1}>
-            <img src={logo} alt="Fakeflix logo" className="brand-logo" />
-          </Col>
-          <Col md={10} className="navigation">
-            <nav className="main-navigation">
+      <header className="header">
+        {/* <Navbar variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img src={logo} alt="Fakeflix logo" className="brand-logo" />
+            </Navbar.Brand>
+            <Nav className="me-auto">
               <NavLink activeclassname="active" to="/">
                 Home
               </NavLink>
@@ -32,14 +32,46 @@ const Header = () => {
               <NavLink activeclassname="active" to="/mylist">
                 My list
               </NavLink>
-            </nav>
-          </Col>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Row>
           <Col md={1} className="user-header-icons">
             <BsSearch className="user-header-icon" />
             <BsFillBellFill className="user-header-icon" />
             <BsFillPersonFill className="user-header-icon" />
           </Col>
-        </Row>
+        </Row> */}
+
+        <Navbar variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img src={logo} alt="Fakeflix logo" className="brand-logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <NavLink activeclassname="active" to="/">
+                  Home
+                </NavLink>
+                <NavLink activeclassname="active" to="/movies">
+                  Movies
+                </NavLink>
+                <NavLink activeclassname="active" to="/tvseries">
+                  TV Series
+                </NavLink>
+                <NavLink activeclassname="active" to="/mylist">
+                  My list
+                </NavLink>
+                <Col className="user-header-icons d-flex">
+                  <BsSearch className="user-header-icon" />
+                  <BsFillBellFill className="user-header-icon" />
+                  <BsFillPersonFill className="user-header-icon" />
+                </Col>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
     </>
   );
