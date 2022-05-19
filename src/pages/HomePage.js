96ef7861ac6a6076c53f4cc331688b390ecd59fc
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-05-19 17:17:15 +0200

    I added apicall co set a random movie and pass it via the props to header component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,12 +7,23 @@ import Hero from "../components/Hero/Hero";
 // Api calls helpers
 import { MOVIE_GENRES } from "../apicalls/apicalls";
 
+const RANDOM_MOVIE_ID = Math.round(Math.random() * 10000);
+console.log(RANDOM_MOVIE_ID);
+
 export function HomePage() {
   const [recentMovies, setRecentMovies] = useState([]);
   const [horrorMovies, setHorrorMovies] = useState([]);
   const [comedyMovies, setComedyMovies] = useState([]);
+  let [randomMovie, setRandomMovie] = useState({});
 
   const fetchRecentMovies = async () => {
+    // set randomMovie
+    await fetch(
+      `https://api.themoviedb.org/3/movie/${RANDOM_MOVIE_ID}?api_key=${process.env.REACT_APP_MOVIES_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => setRandomMovie((randomMovie = { ...data })))
+      .catch((error) => console.log("error: ", error));
     // set recentMovies
     await fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIES_API_KEY}`
@@ -42,7 +53,7 @@ export function HomePage() {
 
   return (
     <>
-      <Hero />
+      <Hero randomMovie={randomMovie}/>
       <main>
         <MovieRow rowTitle={"Recent Movies"} movies={recentMovies} />
         <MovieRow rowTitle={"Comedy Movies"} movies={comedyMovies} />
