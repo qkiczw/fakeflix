42a02e814dbcaed3ce4a3443ee520212cb4a8aa8
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-05-30 18:48:50 +0200

    I added functions to show filtered movies or all movies from the api

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -3,17 +3,33 @@ import React, { useState } from "react";
 // Components
 import MovieCard from "../components/MovieCard/MovieCard;";
 // Bootstrap components
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Button } from "react-bootstrap";
 
 export function Movies({ movies }) {
   const [isFiltered, setFileterd] = useState(true);
   const [movieGenre, setMovieGenre] = useState(35);
 
-  // use the filtered movies from props and try filter with categories with filtered array;
+  const showCategorizedByGenre = (genre) => {
+    setMovieGenre(genre);
+    setFileterd(true);
+  };
+  const showAllMovies = () => {
+    setFileterd(false);
+  };
 
   return (
     <>
       <main>
+        <h3>Category</h3>
+        <Button variant="danger" onClick={() => showCategorizedByGenre(27)}>
+          Horror
+        </Button>
+        <Button variant="danger" onClick={() => showCategorizedByGenre(35)}>
+          Comedy
+        </Button>
+        <Button variant="success" onClick={() => showAllMovies()}>
+          All Movies
+        </Button>
         <Container fluid>
           <h2>Movies page</h2>
 
