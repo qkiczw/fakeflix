4446852911f11604d828e28fef042546f93a6988
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-06-08 18:55:55 +0200

    I added some css to the search component

--- src/components/SearchComponent/searchComponent.js
+++ src/components/SearchComponent/searchComponent.js
@@ -3,16 +3,24 @@ import React from "react";
 // icons
 import { BsSearch } from "react-icons/bs";
 
+const activeInputHandler = (e) => {
+  e.preventDefault();
+  const searchContainer = document.querySelector(".search-container");
+  const searchInput = document.querySelector(".search-input");
+  searchInput.classList.toggle("search-input-active");
+  searchContainer.classList.toggle("search-container-active");
+};
+
 const SearchComponent = () => {
   return (
     <>
       <form className="search-container">
         <label htmlFor="search-input"></label>
-        <input id="search-input" placeholder="type a title"></input>
+        <input id="search-input" className="search-input" />
+        <button onClick={activeInputHandler} className="search-input-button">
+          <BsSearch className="search-input-button-icon user-icon" />
+        </button>
       </form>
-      <button>
-        <BsSearch className="search-input-icon" />
-      </button>
     </>
   );
 };
