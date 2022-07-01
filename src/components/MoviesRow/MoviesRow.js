f092afdf9a5a09658b5113ab00f92826b277fcd0
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-07-01 17:40:17 +0200

    A test slider was added to the project to check funcionality

--- src/components/MoviesRow/MoviesRow.js
+++ src/components/MoviesRow/MoviesRow.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+// Reac Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
 // Components
 import MovieCard from "../MovieCard/MovieCard;";
 import SeeAllButton from "../SeeAllButton/SeeAllButton";
@@ -18,11 +26,25 @@ const MoviesRow = ({ movies, rowTitle }) => {
           </Col>
         </Row>
         <Row>
-          {movies
+          {/* {movies
             .map((movie) => <MovieCard key={movie.id} movieData={movie} />)
             .filter((movie, index) => {
               return index <= 5;
-            })}
+            })} */}
+          <Swiper
+            modules={[Navigation]}
+            loop={true}
+            navigation={true}
+            spaceBetween={50}
+            slidesPerView={6}
+          >
+            {movies.map((movie) => (
+              <SwiperSlide key={movie.id} movieData={movie}>
+                <h4>{movie.title}</h4>
+                <p>movie.id</p>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </Row>
       </Container>
     </section>
