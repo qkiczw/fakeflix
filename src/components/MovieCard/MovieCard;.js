5e8c20eadd3518f5bdf689adef10b17153580bb8
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-07-05 17:34:20 +0200

    I added buttons under movie thumb but for now they are not doing anything

--- src/components/MovieCard/MovieCard;.js
+++ src/components/MovieCard/MovieCard;.js
@@ -6,9 +6,15 @@ import genericThumb from "../../assets/genericThumb.jpg";
 // Icons
 import {
   BsPlayCircleFill,
-  BsArrowDownCircle,
-  BsPlusCircle,
+  BsCheckCircle,
+  BsHandThumbsUp,
 } from "react-icons/bs";
+
+import { AiOutlineDownCircle } from "react-icons/ai";
+
+const testFn = () => {
+  console.log("I am doing something!");
+};
 
 const MovieCard = (props) => {
   const { movieData } = props;
@@ -61,11 +67,21 @@ const MovieCard = (props) => {
           <div className="movie-card-title">
             {movieData.title ? movieData.title : movieData.name}
           </div>
-          <div className="movie-card-desc">
+          <div className="movie-card-buttons">
+            <span className="movie-card-buttons-left">
+              <BsPlayCircleFill className="movie-card-button" />
+              <BsCheckCircle className="movie-card-button" />
+              <BsHandThumbsUp className="movie-card-button" />
+            </span>
+            <span className="movie-card-buttons-right">
+              <AiOutlineDownCircle className="movie-card-button" />
+            </span>
+          </div>
+          {/* <div className="movie-card-desc">
             {movieData.desc
               ? movieData.desc
               : movieData.overview.substring(0, 100)}
-          </div>
+          </div> */}
         </div>
       </div>
     </>
