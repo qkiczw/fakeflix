e08ad12eb67a7212e48293cf2c39e40bfa2b4ca5
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-07-21 18:13:19 +0200

    Change css to sass and class names to bem methodology in Search Component.

--- src/components/SearchComponent/searchComponent.js
+++ src/components/SearchComponent/searchComponent.js
@@ -5,20 +5,20 @@ import { BsSearch } from "react-icons/bs";
 
 const activeInputHandler = (e) => {
   e.preventDefault();
-  const searchContainer = document.querySelector(".search-container");
-  const searchInput = document.querySelector(".search-input");
-  searchInput.classList.toggle("search-input-active");
-  searchContainer.classList.toggle("search-container-active");
+  const searchContainer = document.querySelector(".search");
+  const searchInput = document.querySelector(".search__input");
+  searchInput.classList.toggle("search__input--active");
+  searchContainer.classList.toggle("search--active");
 };
 
 const SearchComponent = () => {
   return (
     <>
-      <form className="search-container">
-        <label htmlFor="search-input"></label>
-        <input id="search-input" className="search-input" />
-        <button onClick={activeInputHandler} className="search-input-button">
-          <BsSearch className="search-input-button-icon user-icon" />
+      <form className="search">
+        <label htmlFor="search__input"></label>
+        <input id="search__input" className="search__input" />
+        <button onClick={activeInputHandler} className="search__input__button">
+          <BsSearch className="search__input__icon user__icon" />
         </button>
       </form>
     </>
