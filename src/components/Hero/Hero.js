474154a215a09882f9d9cb56037ff5dc99b64c6b
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-07-21 17:53:18 +0200

    Change styles from css to scss and names to bem methodology

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,17 +15,17 @@ const Hero = ({ randomMovie }) => {
   return (
     <Row>
       <Col xs={12}>
-        <section className="hero">
+        <div className="hero">
           <img
-            className="hero-bacground"
+            className="hero__background"
             src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
             alt={overview}
           />
-          <Col className="hero-desc-and-btns">
-            <h2 className="hero-title">{title ? title : ""}</h2>
-            <p className="hero-overview">{overview ? overview : ""}</p>
-            <Col className="hero-btns" xs={12} md={5} lg={4}>
-              <Button variant="light" size="lg" className="hero-btn">
+          <Col className="hero__description">
+            <h2 className="hero__title">{title ? title : ""}</h2>
+            <p className="hero__overview">{overview ? overview : ""}</p>
+            <Col className="hero__btns" xs={12}>
+              <Button variant="light" size="lg" className="hero__btn">
                 <BsCaretRightSquare />
                 Watch Trailer
               </Button>
@@ -33,14 +33,14 @@ const Hero = ({ randomMovie }) => {
                 onClick={() => moreInfoStateHandler(randomMovie)}
                 variant="outline-light"
                 size="lg"
-                className="hero-btn"
+                className="hero__btn"
               >
                 <BsInfoSquareFill />
                 More Info
               </Button>
             </Col>
           </Col>
-        </section>
+        </div>
       </Col>
     </Row>
   );
