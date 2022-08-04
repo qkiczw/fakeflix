d3bead6c58a0a97bed9ffae4fd1dd753ffe397d1
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-08-04 19:28:47 +0200

    Add some code to display movies id on a myslist page.

--- src/pages/MyList.js
+++ src/pages/MyList.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Bootstrap components
 import { Container, Row } from "react-bootstrap";
 
 export function MyList() {
+  const [myMoviesList, setMyMoviesList] = useState(
+    localStorage.getItem("myMoviesList")
+  );
+
   return (
     <>
       <main>
         <Container fluid className="movies-page">
-          <Row></Row>
+          <Row>
+            {JSON.parse(myMoviesList).map((item) => (
+              <p>Movie ID {item}</p>
+            ))}
+            {/* MyMoviesList has to be an array of objects with movie data */}
+          </Row>
         </Container>
       </main>
     </>
