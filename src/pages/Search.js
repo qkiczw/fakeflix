9dbe831b545294a008da9ee7e81051a78abb7316
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-08-29 17:55:29 +0200

    Add MovieCard component to display searched movies.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -12,16 +12,17 @@ export function Search({ searchedMovieData, allMoviesFiltered }) {
     <>
       <main>
         <Container fluid className="movies-page">
-          <h2 gap={3}>Search results:</h2>
+          <h2 gap={3} className="mb-5">
+            You are searching for: {searchedMovieData}
+          </h2>
           <Row>
-            <p>{searchedMovieData}</p>
-            <ul>
-              {allMovies
-                .filter((movie) => movie.title.includes(searchedMovieData))
-                .map((movie) => (
-                  <li key={movie.id}>{movie.title}</li>
-                ))}
-            </ul>
+            {allMovies
+              .filter((movie) => movie.title.includes(searchedMovieData))
+              .map((movie) => (
+                <Col key={movie.id} xs={6} md={4} lg={2} className="mb-3">
+                  <MovieCard movieData={movie} />
+                </Col>
+              ))}
           </Row>
         </Container>
       </main>
