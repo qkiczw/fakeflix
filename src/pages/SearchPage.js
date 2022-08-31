fd424a97f6d2e53efb2c4de567d9d67734de97c1
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-08-31 17:53:55 +0200

    Add function to change all letters in title to lowerCase.

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -6,9 +6,12 @@ import MovieCard from "../components/MovieCard/MovieCard;";
 // Bootstrap components
 import { Container, Row, Col } from "react-bootstrap";
 
+// Utils
+import { changeTitleToLowerCase } from "../utils/helpers";
+
 export function Search({ searchedMovieData, allMoviesFiltered }) {
   const [allMovies, setAllMovies] = useState(allMoviesFiltered);
-  const [searchedMovie, setSearchedMovie] = useState("xxx");
+  const [searchedMovie, setSearchedMovie] = useState("");
 
   // TODO: Change all titles, cast to lower case
 
@@ -25,7 +28,11 @@ export function Search({ searchedMovieData, allMoviesFiltered }) {
           </h2>
           <Row>
             {allMovies
-              .filter((movie) => movie.title.includes(searchedMovie))
+              .filter((movie) =>
+                changeTitleToLowerCase(movie.title).includes(
+                  changeTitleToLowerCase(searchedMovie)
+                )
+              )
               .map((movie) => (
                 <Col key={movie.id} xs={6} md={4} lg={2} className="mb-3">
                   <MovieCard movieData={movie} />
