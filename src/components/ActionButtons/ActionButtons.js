c8eb9a0e784b9347b08822b397ae4c23bfad0c73
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-09-22 18:57:05 +0200

    Add state to change the icon if the movie is on the list or not

--- src/components/ActionButtons/ActionButtons.js
+++ src/components/ActionButtons/ActionButtons.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { handleLocalStorageData } from "../../utils/helpers";
 
@@ -13,19 +13,29 @@ const ActionButtons = ({ movieData }) => {
   let localStorageMoviesList =
     JSON.parse(localStorage.getItem("myMoviesList")) ?? [];
 
+  const [isMovieOnTheList, setMovieOnTheList] = useState(
+    localStorageMoviesList.some((movie) => movie.id === movieData.id)
+  );
+
   return (
     <>
       <div className="actionButtons">
         <div className="actionButtons__button">
-          {localStorageMoviesList.some((movie) => movie.id === movieData.id) ? (
+          {isMovieOnTheList ? (
             <BsCheckCircleFill
               className="actionButton"
-              onClick={() => handleLocalStorageData(movieData)}
+              onClick={() => {
+                handleLocalStorageData(movieData);
+                setMovieOnTheList(!isMovieOnTheList);
+              }}
             />
           ) : (
             <BsPlusCircle
               className="actionButton"
-              onClick={() => handleLocalStorageData(movieData)}
+              onClick={() => {
+                handleLocalStorageData(movieData);
+                setMovieOnTheList(!isMovieOnTheList);
+              }}
             />
           )}
           {/* TODO: if statement with icons (thumb down if rating is bad, thumb up if it is average and two thumbs up is is good) */}
