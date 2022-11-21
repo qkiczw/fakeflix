a883455333d55ffcac9c9120a644c2c8d8c61a18
Lukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-11-21 17:54:20 +0100

    Minor change - delete react fragment.

--- src/components/Providers/Providers.jsx
+++ src/components/Providers/Providers.jsx
@@ -2,24 +2,22 @@ import React from "react";
 
 const Providers = ({ movieProviders }) => {
   const providers = movieProviders.flatrate || [];
-    
+
   return (
-    <>
-      <div className="mt-5 mb-5 providers">
-        <h3 className="providers__title">Available on:</h3>
-        <ul className="providers__list mt-4">
-          {providers.map((provider) => (
-            <li key={provider.provider_id} className="providers provider">
-              <img
-                    className="provider__logo"
-                    src={`https://image.tmdb.org/t/p/original${provider.logo_path}`}
-                    alt={provider.provider_name}
-                  />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div className="mt-5 mb-5 providers">
+      <h3 className="providers__title">Available on:</h3>
+      <ul className="providers__list mt-4">
+        {providers.map((provider) => (
+          <li key={provider.provider_id} className="providers provider">
+            <img
+              className="provider__logo"
+              src={`https://image.tmdb.org/t/p/original${provider.logo_path}`}
+              alt={provider.provider_name}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
