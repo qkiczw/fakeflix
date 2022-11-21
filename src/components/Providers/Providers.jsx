00dbca4590faca6151fa8725111afec2b85acbf6
Lukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-11-21 17:08:59 +0100

    Add some styles to providers component and delete unnecessary code.

--- src/components/Providers/Providers.jsx
+++ src/components/Providers/Providers.jsx
@@ -2,22 +2,18 @@ import React from "react";
 
 const Providers = ({ movieProviders }) => {
   const providers = movieProviders.flatrate || [];
-    console.log(
-      `providers: `, providers
-      // providers.map((provider) => provider.provider_name)
-    );
+    
   return (
     <>
       <div className="mt-5 mb-5 providers">
         <h3 className="providers__title">Available on:</h3>
-        <ul className="providers__list">
+        <ul className="providers__list mt-4">
           {providers.map((provider) => (
-            <li key={provider.provider_id} className="providers__provider">
-             
+            <li key={provider.provider_id} className="providers provider">
               <img
-                    className="logos__imdb"
+                    className="provider__logo"
                     src={`https://image.tmdb.org/t/p/original${provider.logo_path}`}
-                    alt={"xxx"}
+                    alt={provider.provider_name}
                   />
             </li>
           ))}
