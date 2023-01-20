9b9aa35a0a5257d80127b4421942bc35ff345ba2
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-01-20 19:37:38 +0100

    Add ternary operator to check if the movie is available or not.

--- src/components/Providers/Providers.jsx
+++ src/components/Providers/Providers.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// Icons
+import { BsFillEmojiFrownFill } from "react-icons/bs";
+
 const Providers = ({ movieProviders }) => {
   const providers = movieProviders.flatrate || [];
 
@@ -7,15 +10,22 @@ const Providers = ({ movieProviders }) => {
     <div className="mt-5 mb-5 providers">
       <h3 className="providers__title">Available on:</h3>
       <ul className="providers__list mt-4">
-        {providers.map((provider) => (
-          <li key={provider.provider_id} className="providers provider">
-            <img
-              className="provider__logo"
-              src={`https://image.tmdb.org/t/p/original${provider.logo_path}`}
-              alt={provider.provider_name}
-            />
-          </li>
-        ))}
+        {!providers[0] ? (
+          <span>
+            <BsFillEmojiFrownFill /> Sorry, the movie is unavailable on any
+            platform
+          </span>
+        ) : (
+          providers.map((provider) => (
+            <li key={provider.provider_id} className="providers provider">
+              <img
+                className="provider__logo"
+                src={`https://image.tmdb.org/t/p/original${provider.logo_path}`}
+                alt={provider.provider_name}
+              />
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
